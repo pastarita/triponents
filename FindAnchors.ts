c0d219986ddcff2855e19a71bf61e749b8cd5a12
Patrick Astarita <patrickastartia@gmail.com>
2024-10-16 20:34:09 -0400

    added pdf spec detailing shapes to render

---
+++ FindAnchors.ts
@@ -0,0 +1,26 @@
+// Find Coordination PointsAnchors
+
+const leftLower: number  = ll
+const leftMiddle = lm
+const leftUpper = lu
+const rightLower = rl
+const rightMiddle = rm
+const rightUpper = ru
+
+
+import md as MarginOffset from './TriangleFlipTranslate'
+
+
+let ll = (ptA - md)
+    
+let lm = (ptD - md)
+
+leftUpper = lu
+
+rightLower = rl
+
+rightMiddle = rm
+
+rightUpper = ru
+
+
